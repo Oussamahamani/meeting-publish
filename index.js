3e0a70badfac6d94f966a447ac0c6ea9c7b31a9b
Oussamahamani <oussamahamani2000@gmail.com>
2024-03-18 12:29:17 -0400

    updating the link

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(cors());
 ////:TODO uncomment this to create the task/ you can also update the time you want the task run
 // var task = cron.schedule('0 23 * * 1-5', () => {
 //   // This function will be executed every minute
-//   fetch("https://canvas-meeting-publishing-production.up.railway.app/canvas")
+//   fetch(proccess.env.appLink)
 // }, {timezone: 'America/New_York' // Set timezone to Eastern Time Zone});
 
 // task.start();
